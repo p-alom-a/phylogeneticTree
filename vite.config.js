be26d14da65c0b06dacb2835f1c8c0680119c46c
Paloma <paloma.sanchezc@icloud.com>
2025-06-04 19:35:10 +0200

    Description de vos modifications

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/phylogeneticTree/',
   plugins: [react()],
   server: {
     host: true, // Écouter sur toutes les interfaces réseau
